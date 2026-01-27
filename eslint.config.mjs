84d2b2131a9d4790c656d46eb48ce28145d04a21
Murat Kale <kalemurat92@gmail.com>
2026-01-27 14:38:03 +0300

    eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,57 @@
+// @ts-check
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default tseslint.config(
+    {
+        files: ['**/*.ts'],
+        rules: {
+            ...eslint.configs.recommended.rules,
+        },
+    },
+    ...tseslint.configs.recommended.map((c) => ({ ...c, files: ['**/*.ts'] })),
+    ...tseslint.configs.stylistic.map((c) => ({ ...c, files: ['**/*.ts'] })),
+    ...angular.configs.tsRecommended.map((c) => ({ ...c, files: ['**/*.ts'] })),
+    {
+        files: ['**/*.ts'],
+        processor: angular.processInlineTemplates,
+        rules: {
+            '@angular-eslint/directive-selector': [
+                'error',
+                {
+                    type: 'attribute',
+                    prefix: 'app',
+                    style: 'camelCase',
+                },
+            ],
+            '@angular-eslint/component-selector': [
+                'error',
+                {
+                    type: 'element',
+                    prefix: 'app',
+                    style: 'kebab-case',
+                },
+            ],
+            '@angular-eslint/component-class-suffix': 'off',
+            '@typescript-eslint/naming-convention': [
+                'error',
+                {
+                    selector: 'interface',
+                    format: ['PascalCase'],
+                    prefix: ['I'],
+                },
+                {
+                    selector: 'variable',
+                    modifiers: ['const'],
+                    format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+                },
+            ],
+            '@typescript-eslint/no-explicit-any': 'error',
+        },
+    },
+    ...angular.configs.templateRecommended.map((c) => ({ ...c, files: ['**/*.html'] })),
+    ...angular.configs.templateAccessibility.map((c) => ({ ...c, files: ['**/*.html'] })),
+    eslintPluginPrettierRecommended
+);
